3bfa2b25403ecfb1534bbc1f1b403e0167045924
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-21 18:35:28 +0700

    perbaikan pada static file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fileUpload = require('express-fileupload');
 const path=require('path');
 
 app.use(express.json());
-app.use(path.join(__dirname,'public'));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
 app.use(fileUpload({
