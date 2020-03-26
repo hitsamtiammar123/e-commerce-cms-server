0be434c4664d4563948536dff45af481383b8426
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-26 15:59:16 +0700

    add scrapper supaya bisa dinamis

--- scrapper.js
+++ scrapper.js
@@ -1,6 +1,9 @@
 const axios=require('axios')
 const fs=require('fs')
 
+var url=process.argv.slice(2)[0]
+console.log({url})
+
 function getDescription(d){
     let des=''
     let colorInfo=d.productInfo[0].productContent.colorDescription
@@ -11,7 +14,6 @@ function getDescription(d){
 
 async function startScrap(){
     try{
-        let url='https://api.nike.com/product_feed/rollup_threads/v2?filter=marketplace%28ID%29&filter=language%28en-GB%29&filter=employeePrice%28true%29&filter=attributeIds%287817e756-7721-4cfb-b404-04df79c685e4%2C16633190-45e5-4830-a068-232ac7aea82c%2C7baf216c-acc6-4452-9e07-39c2ca77ba32%29&anchor=0&consumerChannelId=d9a5bc42-4b9c-4976-858a-f159cf99c647&count=60';
         let {data}=await axios.get(url)
         let dataObj=data
         let result=[];
@@ -39,6 +41,7 @@ async function startScrap(){
     }catch(err){
         console.log(err)
         console.log('Ada error di pada saat nge-scrap',err)
+        process.exit(-1)
     }
 }
 
