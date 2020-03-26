1065e87f4585609b220af5852aa6a4effcf35b11
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-26 12:45:45 +0700

    scrap with description

--- scrapper.js
+++ scrapper.js
@@ -1,9 +1,17 @@
 const axios=require('axios')
 const fs=require('fs')
 
+function getDescription(d){
+    let des=''
+    let colorInfo=d.productInfo[0].productContent.colorDescription
+    des=colorInfo
+
+    return des
+}
+
 async function startScrap(){
     try{
-        let url='https://api.nike.com/cic/browse/v1?queryid=products&anonymousId=C5F26689B209BC15034D7F00E9D08BE1&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(ID)%26filter%3Dlanguage(en-GB)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c)%26anchor%3D48%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D50';
+        let url='https://api.nike.com/cic/browse/v1?queryid=products&anonymousId=C5F26689B209BC15034D7F00E9D08BE1&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(ID)%26filter%3Dlanguage(en-GB)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c)%26anchor%3D48%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D60';
         let {data}=await axios.get(url)
         let dataObj=data
         let result=[];
@@ -19,7 +27,7 @@ async function startScrap(){
             obj.category=categories[Math.floor(Math.random()*categories.length)]
             obj.image_url=d.publishedContent.properties.productCard.properties.squarishURL
             obj.isScrapped=true
-            obj.description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium asperiores provident quidem, maxime recusandae voluptate incidunt quo cumque harum? Dolorum, enim corrupti quod eum fuga officia hic commodi nam ducimus!'
+            obj.description=getDescription(d)
             result.push(obj)
             
         }
@@ -29,6 +37,7 @@ async function startScrap(){
         fs.writeFileSync(file,file_content)
 
     }catch(err){
+        console.log(err)
         console.log('Ada error di pada saat nge-scrap',err)
     }
 }
